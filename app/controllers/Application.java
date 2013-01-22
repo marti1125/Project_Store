d2c87ec4948f9b626ec233c9f6dd1be71027a5c9
Willy Aguirre <marti1125@gmail.com>
2013-01-21 21:34:02 -0500

    add page contact

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -12,5 +12,10 @@ public class Application extends Controller {
     public static void index() {
         render();
     }
+    
+    public static void contact(){
+    	render();
+    }
+    
 
 }
