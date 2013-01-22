8ae6cb83f561f3299c05be9133b18a6b7ab27d06
Willy Aguirre <marti1125@gmail.com>
2013-01-22 10:45:44 -0500

    add theme for contactenos

--- app/controllers/Mails.java
+++ app/controllers/Mails.java
@@ -7,24 +7,24 @@ import play.libs.Mail;
 import play.mvc.*;
 import java.util.*;
 
-public class Mails extends Mailer{
-	
-	public static void message(String address, String message){
+public class Mails extends Mailer {
+
+	public static void message(String address, String message) {
 		try {
-			
+
 			SimpleEmail email = new SimpleEmail();
 			email.setFrom(address);
 			email.addTo("marti1125@gmail.com");
 			email.setSubject("Consulta");
 			email.setMsg(message);
-			Mail.send(email);			
+			Mail.send(email);
 			System.out.println("Send message was successful");
-			
+
 		} catch (Exception e) {
 			System.out.println("Error...");
 			System.out.println(e);
 		}
-		
+
 	}
 
 }
