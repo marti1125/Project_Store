2d4bbdad17323021f4c6d097199c2070939c0ff0
Willy Aguirre <marti1125@gmail.com>
2013-01-23 00:18:15 -0500

    add products.html and supplers.html

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,15 +9,22 @@ import models.*;
 
 public class Application extends Controller {
 
-    public static void index() {
-        render();
-    }
-    
-    public static void contact(String address, String message){
-    	Mails mail = new Mails();
-    	mail.message(address, message);
-    	render();
-    }
-    
+	public static void index() {
+		render();
+	}
+
+	public static void contact(String address, String message) {
+		Mails mail = new Mails();
+		mail.message(address, message);
+		render();
+	}
+
+	public static void suppliers() {
+		render();
+	}
+	
+	public static void products() {
+		render();
+	}
 
 }
