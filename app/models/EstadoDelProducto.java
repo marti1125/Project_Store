0b88ba0aadbb0219406986665d53093affda2a85
Willy Aguirre <marti1125@gmail.com>
2013-11-03 23:31:22 -0500

    muestra los productos agotados

--- app/models/EstadoDelProducto.java
+++ app/models/EstadoDelProducto.java
@@ -16,4 +16,14 @@ public class EstadoDelProducto extends Model{
 	
 	@Enumerated(EnumType.STRING)
 	public Estado estado;
+	
+	public String getEstado(){
+		if(estado.toString() == "V"){
+			return Estado.V.descripcion;
+		} else if(estado.toString() == "P") {
+			return Estado.P.descripcion;
+		} else {
+			return "";
+		}
+	}
 }
