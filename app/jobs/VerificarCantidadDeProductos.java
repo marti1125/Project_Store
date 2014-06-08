9c2b7b7fb53651af56bc0a3cf3d43d00f3877f1e
Willy Aguirre <marti1125@gmail.com>
2014-06-08 14:50:04 -0500

    se retira  job (momentanio) #5

--- app/jobs/VerificarCantidadDeProductos.java
+++ app/jobs/VerificarCantidadDeProductos.java
@@ -8,17 +8,17 @@ import models.Producto;
 import models.EstadoDelProducto;
 import play.jobs.*;
 
-@Every("1mn")
+//@Every("1mn")
 public class VerificarCantidadDeProductos extends Job {
 	
-	public void doJob() {	
+	/*public void doJob() {	
 		
 		List<Producto> productos = Producto.findAll();
 		for(Producto p : productos){
 			Producto productoEncontrado = Producto.findById(p.codigo);
 			EstadoDelProducto estadoProducto = new EstadoDelProducto();
 			EstadoDelProducto existeProducto = EstadoDelProducto.find("producto.codigo",p.codigo).first();
-			/*if(p.cantidadInicial == 0 && existeProducto == null){
+			if(p.cantidadInicial == 0 && existeProducto == null){
 				estadoProducto.producto = productoEncontrado;
 				estadoProducto.estado = Estado.V;
 				estadoProducto.save();
@@ -28,9 +28,9 @@ public class VerificarCantidadDeProductos extends Job {
 				estadoProducto.save();
 			} else {
 				// Hay Stock
-			}*/
+			}
 		}
 		
-	}
+	}*/
 
 }
